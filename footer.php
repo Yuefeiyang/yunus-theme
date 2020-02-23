c91c1d3e7a0d08b132673614e84177545ec174cf
= <=>
2020-02-22 21:01:13 -0800

    minor update

--- footer.php
+++ footer.php
@@ -15,7 +15,6 @@
 
 	<footer id="colophon" class="site-footer">
 		<div class="site-info">
-		test
 			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'yunus-theme' ) ); ?>">
 				<?php
 				/* translators: %s: CMS name, i.e. WordPress. */
